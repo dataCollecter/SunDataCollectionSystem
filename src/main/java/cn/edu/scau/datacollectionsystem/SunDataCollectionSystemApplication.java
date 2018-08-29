751f2ad9a0ae130b8da93e13a4ca5eec76843d75
ASun <liurf923@gmail.com>
2018-08-29 22:33:36 +0800

    Init Commit

---
+++ src/main/java/cn/edu/scau/datacollectionsystem/SunDataCollectionSystemApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.scau.datacollectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunDataCollectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunDataCollectionSystemApplication.class, args);
+    }
+}
